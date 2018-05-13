3cf425f2daafb5b5e256d60973dce00273fe5e9c
Tumble <tumblegamer@gmail.com>
2018-05-13 15:12:37 +0100

    detection of running webserver

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const http = require('http');
 
 function setupServer(app, port=3000) {
+  if (global.tnserver) {
+    return global.tnserver;
+  }
   port = process.env.PORT || port;
   var server = http.Server(app);
 
   server.listen(port, function listenAction() {
     console.log("Server listening on port " + port);
   });
+  global.tnserver = server;
   return server;
 }
 
